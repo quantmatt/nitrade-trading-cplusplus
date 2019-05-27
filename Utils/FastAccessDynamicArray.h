98e413261d814135cb5edd989261d9070afdb2a8
quantmatt <matthew.bailey@enzymelabs.com.au>
2019-05-27 15:13:24 +1000

    added Utils to project

---
+++ Utils/FastAccessDynamicArray.h
@@ -0,0 +1,67 @@
+#pragma once
+//Vectors can be a bit slow so this is a lightweight array using unique_ptr
+//that allows fast access to the array elements but slow insertion
+namespace Utils {
+
+	//interface for mocking
+	template<class T>
+	class IFastAccessDynamicArray
+	{
+	public:
+		IFastAccessDynamicArray() {}
+		virtual ~IFastAccessDynamicArray() {}
+
+		virtual void add(T value) = 0;
+		virtual T& get(int pos) = 0;
+	};
+
+
+	template<class T>
+	class FastAccessDynamicArray :
+		public IFastAccessDynamicArray<T>
+	{
+	private:
+		int _pos{ 0 };
+		int _size{ 0 };
+		std::unique_ptr<T[]> _values;
+
+	public:
+
+		FastAccessDynamicArray() = default;
+
+		virtual ~FastAccessDynamicArray() = default;
+
+		int size() {
+			return _size;
+		}
+		void add(T value)
+		{
+			//create a temp array one size larger
+			auto temp = std::make_unique<T[]>(_size + 1);
+
+			//copy the pointers across
+			for (int i = 0; i < _size; i++)
+				temp[i] = std::move(_values[i]);
+
+			std::exchange(_values, std::move(temp));
+
+			//add in the new item and increment the counter
+			_values[_size] = std::move(value);
+			_size++;
+
+
+		}
+
+		T & get(int pos)
+		{
+			//don't check pos is in range - we want fast access, caller is responsible for sending correct pos						
+			return _values[pos];
+		}
+
+		T& operator [](int pos) { return _values[pos]; }
+
+
+	};
+
+
+}
---
+++ Utils/utils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "IOIterator.h"
+#include "FastAccessDynamicArray.h"
+#include "SeriesBuffer.h"
